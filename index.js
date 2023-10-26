68f94a9bb9838389f61a30c5421c177f4ecb2121
KWensmann <kurtwensmann@gmail.com>
2023-10-26 09:10:06 -0500

    array of questions in index.js

--- index.js
+++ index.js
@@ -30,27 +30,28 @@ const questions = [
     name: 'licenses',
     choices: ['Apache 2.0', 'MIT', 'ISC', 'BSD 3-Clause', 'BSD 2-Clause', 'GNU GPL v3']
   },
-  // {
-  //   type: 'input',
-  //   message: 'What command should be run to install dependencies?',
-  //   name: 'installDepends'
-  // },
-  // {
-  //   type: 'input',
-  //   message: 'What command should be run to run tests?',
-  //   name: 'runTests'
-  // },
-  // {
-  //   type: 'input',
-  //   message: 'What does the user need to know about using the repo?',
-  //   name: 'repoUseInfo'
-  // },
-  // {
-  //   type: 'input',
-  //   message: 'What does the user need to know about contributing to the repo?',
-  //   name: 'contributionInfo'
-  // }
+  {
+    type: 'input',
+    message: 'What command should be run to install dependencies?',
+    name: 'installDepends'
+  },
+  {
+    type: 'input',
+    message: 'What command should be run to run tests?',
+    name: 'runTests'
+  },
+  {
+    type: 'input',
+    message: 'What does the user need to know about using the repo?',
+    name: 'repoUseInfo'
+  },
+  {
+    type: 'input',
+    message: 'What does the user need to know about contributing to the repo?',
+    name: 'contributionInfo'
+  }
 ];
+console.log(questions)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
